fb1440ec8afece97a1959bfccc8b2465667e54a6
John Wang <ddevnz@gmail.com>
2016-05-09 22:48:37 +1200

    ADD APP LOADING TIMESTAMP
    Showcasing the use of Angular 2.0 DatePipe to format a date object to string. Timestamp will update on browser refresh.

--- src/AspNg2TsStarter/scripts/app.component.ts
+++ src/AspNg2TsStarter/scripts/app.component.ts
@@ -2,8 +2,15 @@
 // Component metadata decorator (works like an attribute in other programming languages)
 @Component({
     selector: 'app',
-    template: 'Angular component <br />{{test}}'
+    // Demonstrate use of DatePipe to format a date value to string based on medium format
+    template: `
+                <h2>Angular component</h2>
+                <h3>{{test}}</h3>
+                <p>This app was loaded at:{{dateObj | date: \'medium\'}}</p>
+              `
 })
 export class AppComponent {
     test: string = "Test";
+    // dateObj variable that sets the current date
+    dateObj: Date = new Date();
 }
